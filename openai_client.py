c37e21348908d2864cccee656e9fac4ce0066b40
Aytekin97 <aytecha.97@gmail.com>
2024-11-13 23:18:00 -0500

    Create OpenAI client

---
+++ openai_client.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+
+from config import settings
+
+
+class OpenAiClient:
+    def __init__(self):
+        self.client = OpenAI(api_key=settings.openai_api_key)
+        self.model = settings.openai_model
+
+    def query_gpt(self, messages, response_format):
+        completion = self.client.beta.chat.completions.parse(
+            model=self.model, messages=messages, response_format=response_format
+        )
+        return completion.choices[0].message.parsed
